5a502810d75cc0082afc322b733efdbd6055e595
abo_abtal <aboabtalking303@gmail.com>
2024-11-02 20:42:08 +0200

    new appr

--- database/migrations/2024_10_29_184610_create_reaction_types_table.php
+++ database/migrations/2024_10_29_184610_create_reaction_types_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('reaction_types', function (Blueprint $table) {
             $table->id();
             $table->string('type');
-            $table->string('Like');
+            $table->boolean('Like');
             $table->timestamps();
             $table->softDeletes();
         });
